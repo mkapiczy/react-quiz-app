dcc1cc03194108e1c8818219cdd99ad494416bbd
mkapiczy <mix.kap@gmail.com>
2020-01-20 22:34:06 +0100

    Style Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,10 +5,25 @@ const Header: React.FC = () => {
     const activeStyle = {color: "#F15B2A"}
 
     return (
-        <nav>
-            <NavLink to="/" activeStyle={activeStyle} exact>Quiz</NavLink>
-            {" | "}
-            <NavLink to="/list" activeStyle={activeStyle} exact>Quiz List</NavLink>
+        <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+            <a className="navbar-brand" href="#">Quiz App</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse"
+                    aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarCollapse">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item nav-link active">
+                        <NavLink to="/" activeStyle={activeStyle} exact>Quiz</NavLink>
+                    </li>
+                    <li className="nav-item nav-link">
+                        <NavLink to="/list" activeStyle={activeStyle} exact>Quiz List</NavLink>
+                    </li>
+                    <li className="nav-item nav-link">
+                        <NavLink to="/quiz/new" activeStyle={activeStyle} exact>New Quiz</NavLink>
+                    </li>
+                </ul>
+            </div>
         </nav>
     )
 }
