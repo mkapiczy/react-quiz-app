c6e857334908b55cafab35a48bf79ddcceb4b26b
mkapiczy <mix.kap@gmail.com>
2020-01-22 17:34:22 +0100

    Add PageNotFound component for no-matching routes

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import './App.scss';
 import Header from "./Header/Header";
-import {Route} from "react-router-dom"
+import {Route, Switch} from "react-router-dom"
 import Quiz from "./Quiz/Quiz";
 import QuizList from "./QuizList/QuizList";
 import NewQuiz from "./QuizForm/QuizForm";
 import HomePage from "./HomePage/HomePage";
+import PageNotFound from "./PageNotFound/PageNotFound";
 
 const App: React.FC = () => {
     return (
         <div id="app" className="container-fluid">
             <Header/>
             <div className="container-fluid main-content">
-                <Route exact path="/" component={HomePage}/>
-                <Route exact path="/quiz/:quizId" component={Quiz}/>
-                <Route exact path="/quiz-list/" component={QuizList}/>
-                <Route exact path="/new-quiz" component={NewQuiz}/>
+                <Switch>
+                    <Route exact path="/" component={HomePage}/>
+                    <Route exact path="/quiz/:quizId" component={Quiz}/>
+                    <Route exact path="/quiz-list/" component={QuizList}/>
+                    <Route exact path="/new-quiz" component={NewQuiz}/>
+                    <Route component={PageNotFound}/>
+                </Switch>
             </div>
         </div>
     );
