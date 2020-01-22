93d6542e89e48d277c0a093af29c6da46cba6c1e
mkapiczy <mix.kap@gmail.com>
2020-01-22 15:11:39 +0100

    Add simple VictoryView

--- src/components/Quiz/Quiz.tsx
+++ src/components/Quiz/Quiz.tsx
@@ -6,6 +6,7 @@ import {bindActionCreators, Dispatch} from "redux";
 import QuestionDisplay from "./QuestionDisplay/QuestionDisplay";
 import QuizButtonGroup from "./QuizButtonGroup/QuizButtonGroup";
 import QuizHeading from "./QuizHeading/QuizHeading";
+import VictoryView from "./VictoryView/VictoryView";
 
 type PropsFromRedux = ConnectedProps<typeof connectStateAndProps>
 type Props = PropsFromRedux
@@ -13,6 +14,7 @@ const Quiz: React.FC<Props> = (props: Props) => {
     const [currentQuestionIdx, setCurrentQuestionIdx] = useState(0)
     const [selectedAnswer, setSelectedAnswer] = useState<Answer | null>(null)
     const [score, setScore] = useState(0)
+    const [isFinished, setIsFinished] = useState(false)
     const isAnswerCorrect = (answer: Answer | null): boolean => answer !== null && answer.id === props.selectedQuiz.questions[currentQuestionIdx].correctAnswerId
 
     const nextQuestion = () => {
@@ -27,29 +29,39 @@ const Quiz: React.FC<Props> = (props: Props) => {
         }
 
     }
-
     const finishQuiz = () => {
-        alert(`Score ${score}/${props.selectedQuiz.questions.length}`)
+        setIsFinished(true)
+    }
+
+    const resetQuiz = () => {
+        setIsFinished(false)
+        setScore(0)
+        setCurrentQuestionIdx(0)
+        setSelectedAnswer(null)
     }
 
     return (
         <div className="container-fluid">
             <QuizHeading title={props.selectedQuiz.title}/>
-            <div className="container questions">
-                <div className="row">
-                    <QuestionDisplay question={props.selectedQuiz.questions[currentQuestionIdx]}
-                                     totalNumberOfQuestions={props.selectedQuiz.questions.length}
-                                     selectedAnswer={selectedAnswer} isAnswerCorrect={isAnswerCorrect(selectedAnswer)}
-                                     onAnswerSelect={onAnswerSelect}
-                    />
+            {isFinished ?
+                <VictoryView score={score} maximum={props.selectedQuiz.questions.length} resetQuiz={resetQuiz}/> :
+                <div className="container questions">
+                    <div className="row">
+                        <QuestionDisplay question={props.selectedQuiz.questions[currentQuestionIdx]}
+                                         totalNumberOfQuestions={props.selectedQuiz.questions.length}
+                                         selectedAnswer={selectedAnswer}
+                                         isAnswerCorrect={isAnswerCorrect(selectedAnswer)}
+                                         onAnswerSelect={onAnswerSelect}
+                        />
+                    </div>
+                    <div className="row">
+                        <QuizButtonGroup currentQuestionIdx={currentQuestionIdx}
+                                         numberOfQuestions={props.selectedQuiz.questions.length}
+                                         areButtonsDisabled={selectedAnswer === null}
+                                         onNextQuestion={nextQuestion} onFinishQuiz={finishQuiz}/>
+                    </div>
                 </div>
-                <div className="row">
-                    <QuizButtonGroup currentQuestionIdx={currentQuestionIdx}
-                                     numberOfQuestions={props.selectedQuiz.questions.length}
-                                     areButtonsDisabled={selectedAnswer === null}
-                                     onNextQuestion={nextQuestion} onFinishQuiz={finishQuiz}/>
-                </div>
-            </div>
+            }
         </div>
     )
 }
---
+++ src/components/Quiz/VictoryView/VictoryView.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import classNames from "classnames";
+import {NavLink} from "react-router-dom";
+
+interface Props {
+    score: number,
+    maximum: number,
+    resetQuiz: () => void
+}
+
+const VictoryView: React.FC<Props> = (props: Props) => {
+    const percentageScore = props.score / props.maximum * 100
+
+    const progressBarClassNames = classNames(
+        'progress-bar progress-bar-stripped', {
+            'progress-bar-success': percentageScore >= 80,
+            'progress-bar-info': percentageScore >= 60 && percentageScore < 80,
+            'progress-bar-warning': percentageScore >= 40 && percentageScore < 60,
+            'progress-bar-danger': percentageScore < 40
+        }
+    )
+    return (
+        <div className="container-fluid">
+            <div className="h3">Your score: {props.score}/{props.maximum}</div>
+            <div className="progress" style={{height: "2rem"}}>
+                <div className={progressBarClassNames} role="progressbar"
+                     aria-valuenow={percentageScore} aria-valuemin={0} aria-valuemax={100}
+                     style={{width: percentageScore + "%", minWidth: "10%"}}>
+                    {percentageScore}%
+                </div>
+            </div>
+            <div className="container" style={{marginTop: "5vmin"}}>
+                <div className="row">
+                    <div className="col text-center">
+                        <NavLink to="/quiz/" onClick={props.resetQuiz} exact>
+                                Retake the quiz
+                        </NavLink>
+                    </div>
+                    <div className="col text-center">
+                        <NavLink to="/quiz/list" exact>
+                           Go back to quiz list
+                        </NavLink>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+export default VictoryView
